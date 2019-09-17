7c930394d0f3bacf526c9eb2cf159d783be426b9
lyg1071334926 <1071334926@qq.com>
2019-09-17 21:15:20 +0800

    Message

---
+++ lyg1071334926/UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 10, b = 15;
+            int c = Calc.ADD(a, b);
+            Assert.AreEqual(25, c);
+
+        }
+        public class Calc
+        {
+            public static int ADD(int a, int b)
+            {
+                return a + b;
+            }
+        }
+    }
+}
