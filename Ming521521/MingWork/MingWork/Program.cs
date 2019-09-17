54b42c36a4ec1cf5d1cce681cca7f87db14a6959
Ming521521 <2738025911@qq.com>
2019-09-17 21:50:30 +0800

    铭第二次作业

---
+++ Ming521521/MingWork/MingWork/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MingWork
+{
+    class Program
+    {
+        private static string[] operkey = { "+", "-", "*", "/" };
+        private static string CreatThis()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            Random random = new Random();
+            int numcount = random.Next(3, 5);
+            for (int i = 0; i < numcount; i++)
+            {
+                stringBuilder.Append(random.Next(100));
+                stringBuilder.Append(operkey[random.Next(4)]);
+            }
+            stringBuilder.Remove(stringBuilder.Length - 1, 1);
+            return stringBuilder.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+
+            StreamWriter sw = new StreamWriter("question.txt");
+            Console.Write("算术题数:");
+            Stack<string> oper = new Stack<string>();
+            Stack<string> num = new Stack<string>();
+            Random random = new Random();
+            int QuestionNum = Int32.Parse(Console.ReadLine());
+            string str = "";
+            string substr = "";
+            int num_count = 0;//数字字符个数
+            for (int j = 0; j <QuestionNum ; j++)
+            {
+                string question = CreatThis();
+                str =question + "!";
+                try
+                {
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        substr = str.Substring(i, 1);
+                        if (computerUtil.judgenumber(substr))//if (数字）
+                        {
+                            if (num_count == 0)
+                            {
+                                num.Push(substr);
+                            }
+                            else
+                            {
+                                num_count--;
+                            }
+                            if (computerUtil.judgenumber(str.Substring(i + 1, 1)))
+                            {
+                                string temp = num.Pop();
+                                temp += str.Substring(i + 1, 1);
+                                num.Push(temp);
+                                num_count++;
+                            }
+                        }
+                        else if (computerUtil.juderoper(substr))
+                        {
+                            if (oper.Count >= 1)
+                            {
+                                int oper1 = computerUtil.judgeOperLEV(substr);
+                                int oper2 = computerUtil.judgeOperLEV(oper.Peek());
+                                if (oper1 > oper2)
+                                {
+                                    oper.Push(substr);
+                                }
+                                else
+                                {
+                                    for (; oper.Count > 0 && num.Count >= 2; oper.Pop())
+                                    {
+                                        string temp = substr;
+                                        int oper3 = computerUtil.judgeOperLEV(substr);
+                                        int oper4 = computerUtil.judgeOperLEV(oper.Peek());
+                                        if (oper4 < oper3)
+                                        {
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            int temp1 = Convert.ToInt32(num.Peek());
+                                            num.Pop();
+                                            int temp2 = Convert.ToInt32(num.Peek());
+                                            num.Pop();
+                                            num.Push(computerUtil.operate(oper.Peek(), temp1.ToString(), temp2.ToString()).ToString());
+                                        }
+                                    }
+                                    oper.Push(substr);
+                                }
+                            }
+                            else
+                                oper.Push(substr);
+                        }
+                        else if (substr == "!")//结束标志
+                        {
+                            for (; oper.Count > 0 && num.Count >= 2; oper.Pop())
+                            {
+                                int temp1 = Convert.ToInt32(num.Peek());
+                                num.Pop();
+                                int temp2 = Convert.ToInt32(num.Peek());
+                                num.Pop();
+                                num.Push(computerUtil.operate(oper.Peek(), temp1.ToString(), temp2.ToString()).ToString());
+                            }
+                        }
+                    }
+                    if(Int32.Parse(num.Peek())<0)
+                    {
+                        j--;
+                        continue;
+                    }
+                    Console.WriteLine("第"+j+"题:    "+question+"="+num.Peek());
+                    string result = "第" + j + "题:    " + question + "=" + num.Peek();
+                    sw.WriteLine(result);
+                }
+                catch (Exception e)
+                {
+                    Console.Write("机算有错");
+                }
+
+            }
+            sw.Flush();
+            sw.Close();
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Ming521521/MingWork/MingWork/computer.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MingWork
+{
+    class computerUtil
+    {
+
+        /**
+         * 判断是否是数字
+         */
+        public static bool judgenumber(string text)
+        {
+            try
+            {
+                int var1 = Convert.ToInt32(text);
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+        /*
+         * 判断是否是运算符
+         */
+        public static bool juderoper(string text)
+        {
+            if (text.Equals("+") || text.Equals("-") || text.Equals("*") || text.Equals("/"))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        /*
+         * 加减乘除
+         * 
+         */ 
+        public static object add(object a,object b)
+        {
+            int d1 = Int32.Parse(a.ToString());
+            int d2 = Int32.Parse(b.ToString());
+            return d1 + d2;
+        }
+        public static object sub(object a,object b)
+        {
+            int d1 = Int32.Parse(a.ToString());
+            int d2=Int32.Parse(b.ToString());
+            return d2 - d1;
+        }
+        public static object mul(object a,object b)
+        {
+            int d1 = Int32.Parse(a.ToString());
+            int d2 = Int32.Parse(b.ToString());
+            return d1 * d2;
+        }
+        public static object div(object a,object b)
+        {
+            int d1 = Int32.Parse(a.ToString());
+            int d2 = Int32.Parse(b.ToString());
+            return d2 / d1;
+        }
+        /*
+         * 优先级判断
+         */ 
+        public static int judgeOperLEV(string text)
+        {
+            if (text.Equals("+") || text.Equals("-"))
+            {
+                return 2;
+            }
+            else return 3;
+        }
+        /*
+         * 运算
+         */ 
+        public static int operate(string type,string a,string b)
+        {
+            if (type == "+")
+            {
+                return Convert.ToInt32(add(a, b));
+            }
+            else if (type == "-")
+            {
+                return Convert.ToInt32(sub(a, b));
+            }
+            else if (type == "*")
+            {
+                return Convert.ToInt32(mul(a, b));
+            }
+            else if (type == "/")
+            {
+                return Convert.ToInt32(div(a, b));
+            }
+            else
+                return 0;
+        }
+
+
+
+    }
+}
