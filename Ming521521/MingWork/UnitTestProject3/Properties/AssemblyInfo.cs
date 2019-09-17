650efb20dbd855de5c7b39e94d8da7c966a1ad54
Ming521521 <2738025911@qq.com>
2019-09-17 22:58:17 +0800

    铭第二次作业

---
+++ Ming521521/MingWork/UnitTestProject3/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject3")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b8d9c26-70b3-49b1-a495-7f202791cce7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Ming521521/MingWork/UnitTestProject3/computerUtilTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MingWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MingWork.Tests
+{
+    [TestClass()]
+    public class computerUtilTests
+    {
+        [TestMethod()]
+        public void judgenumberTest()
+        {
+            Assert.AreEqual(true,computerUtil.judgenumber("2"));
+        }
+
+        [TestMethod()]
+        public void juderoperTest()
+        {
+            Assert.AreEqual(true,computerUtil.juderoper("+"));
+        }
+
+        [TestMethod()]
+        public void addTest()
+        {
+            Assert.AreEqual(computerUtil.add(2, 3), 5);
+        }
+
+        [TestMethod()]
+        public void subTest()
+        {
+            Assert.AreEqual(computerUtil.sub(2, 3), 1);
+        }
+
+        [TestMethod()]
+        public void mulTest()
+        {
+            Assert.AreEqual(computerUtil.mul(2, 3), 6);
+        }
+
+        [TestMethod()]
+        public void divTest()
+        {
+            Assert.AreEqual(computerUtil.div(2, 4), 2);
+        }
+        [TestMethod()]
+        public void operateTest()
+        {
+            Assert.AreEqual(computerUtil.operate("*","2","3"), 6);
+        }
+    }
+}
