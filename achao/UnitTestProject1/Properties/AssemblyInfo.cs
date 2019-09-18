5c8b0d6ce2b426baaae05674b7290c9cf0300dfa
mengcc1 <793907486@qq.com>
2019-09-18 16:30:09 +0800

    [descriptive message]

---
+++ achao/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bbae0f38-477a-4832-b96a-7f6c2705e13f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ achao/achao/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+using System.Threading;
+
+namespace achao
+{
+    class Program
+    {
+        public static char[] Operator = { '+', '-', '*', '/' };
+        //产生运算式
+        public static string MakeFormula()
+        {
+
+            string result = null;
+            Random random = new Random();
+
+            int Number = (int)random.Next(0, 101);
+            int op_count = (int)random.Next(2, 4);
+            result += Number;
+
+            for (int i = 0; i < random.Next(2, 4); i++)
+            {
+                Thread.Sleep(200);
+                Number = (int)random.Next(0, 101);
+                op_count = (int)random.Next(0, 4);
+                result = result + Operator[op_count] + Number;
+            }
+
+            return result;
+        }
+        //计算运算式
+        public static string Solve(string formula)
+        {
+
+            DataTable dt = new DataTable();
+            object ob = null;
+            ob = dt.Compute(formula, "");
+
+            while (ob.ToString().Contains(".") || formula.Contains("/0"))  //判断是否存在小数和除0操作
+            {
+
+                formula = MakeFormula();
+                ob = dt.Compute(formula, "");
+            }
+            while (Convert.ToInt32(ob) < 0 || ob.ToString().Contains("."))    //结果出现负数或小数，则重新生成
+            {
+                formula = MakeFormula();
+                ob = dt.Compute(formula, "");
+            }
+            return formula + "=" + ob.ToString();
+        }
+        public static void Write(string formula)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(@"C:\Users\Administrator\Desktop\软件工程\AchaoCalculator\结果.txt",true);
+                sw.WriteLine(formula);
+                sw.Close();
+            }
+            catch
+            {
+                Console.WriteLine("保存错误！");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Console.WriteLine("请输入练习题数量：");
+            n = int.Parse(Console.ReadLine());
+            for(int i=1;i<=n;i++)
+            {
+                Program.Write(Solve(MakeFormula()));
+                Console.WriteLine(Solve(MakeFormula()));
+            }
+        }
+            
+        }
+    }
+
