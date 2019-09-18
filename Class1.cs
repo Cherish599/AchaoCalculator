4bf1be5d31e6dcf98f70b62843ffb8c52c96b9f8
cl12312 <54902588+cl12312@users.noreply.github.com>
2019-09-18 21:52:10 +0800

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -5,11 +5,11 @@ using System.Text;
 
 namespace cee
 {
-    class CreatFile
+    class CreatFile//这是一个储存算式到文本文件的一个类
     {
         public CreatFile(string st)
         {
-            StreamWriter sw = new StreamWriter("D:/subject.txt", true);
+            StreamWriter sw = new StreamWriter("D:/subject.txt", true)；
             sw.WriteLine(st);
             sw.Close();
 
