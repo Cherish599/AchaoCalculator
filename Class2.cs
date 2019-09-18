95a14dc7e24fb4b1e1edd40b01136f3756931a14
cl12312 <54902588+cl12312@users.noreply.github.com>
2019-09-18 21:56:05 +0800

    Update Class2.cs

--- Class2.cs
+++ Class2.cs
@@ -11,16 +11,16 @@ namespace cee
         public PrintCalcuate(int n)
         {
             
-            char[] o = { '+', '-', '*', '/' };
+            char[] o = { '+', '-', '*', '/' };//将加减乘除放进数组
             Random rd = new Random();
             int i = 0;
             while (i < n)
             {
                 int h = rd.Next(2, 4);
                 int a = rd.Next(0, 100);
-                string result = null;
+                string result = null;//清空result
                 result = result + a;
-                for (int j = 0; j < h; j++)
+                for (int j = 0; j < h; j++)//一个输出计算式的循环
                 {
                     int m = rd.Next(0, 4);
                     int b = rd.Next(0, 100);
@@ -46,7 +46,7 @@ namespace cee
 
 
                 }
-                if (Com(result) % 1 == 0 && Com(result) >= 0)
+                if (Com(result) % 1 == 0 && Com(result) >= 0)//结果除1如果有余数肯定是小数同时结果不能为负数
                 {
                     i++;
                     Console.WriteLine(result + "=" + Com(result));
@@ -57,7 +57,7 @@ namespace cee
             }
 
         }
-        public double Com(string result)
+        public double Com(string result)//计算运算式的函数
         {
 
             DataTable da = new DataTable();
