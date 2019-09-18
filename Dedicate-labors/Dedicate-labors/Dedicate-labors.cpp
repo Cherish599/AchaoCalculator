ee32e85923ac6525d22b957a929d491ebfe9e602
Your Name <email@example.com>
2019-09-18 20:04:46 +0800

    对四则运算器的编程

---
+++ Dedicate-labors/Dedicate-labors/Dedicate-labors.cpp
@@ -0,0 +1,136 @@
+﻿#include"pch.h"
+#include<vector>
+#include<string>
+#include<iostream>
+#include<cstdlib>
+#include<stack> //栈
+#include<ctime>
+#include"Calculator.h"
+
+#define random(a, b) (rand()%(b-a+1)+a)
+using namespace std;
+
+string Calculator::FH(char formulaChar)
+{
+	char a[4] = { '+', '-', '*', '/' };
+	int i = 0;
+	for (i = 0; i < 4; i++)
+		if (a[i] == formulaChar)
+			break;
+	return op[i];
+}
+
+string Calculator::MakeFormula() {//创建公式
+	string formula = ""; //表面出现的公式
+	srand((unsigned int)time(NULL));
+	int count = random(1, 3); //公式长度
+	int start = 0;//开始计数
+	int number1 = random(1, 10);//第一个数字
+	formula += to_string(number1);
+	while (start <= count) {
+		int operation = random(0, 3);//符号
+		int number2 = random(1, 100);//第二个数字
+		formula += op[operation] + to_string(number2);//符号+数字
+		start++;
+	}
+	return formula;
+}
+
+string Calculator::Solve(string formula) { // 解决公式：方法：将公式转换为后缀式再求解
+	vector<string> *hz = new vector<string>(); //存放后缀表达式
+	stack<string> *fh = new stack<string>(); //存放符号
+	int start = 0, len = 0;//start 开始， len截取长度
+	for (auto formulaChar : formula) {
+		if (formulaChar == '+' || formulaChar == '-' || formulaChar == '*' || formulaChar == '/')
+		{
+			hz->push_back(formula.substr(start, len - start));//ok
+			start = len + 1;//ok
+			if (fh->empty())
+				fh->push(FH(formulaChar));
+			else {
+				if ((fh->top() == "+" || fh->top() == "-") && (formulaChar == '*' || formulaChar == '/'))
+					fh->push(FH(formulaChar)); //ok
+				else
+				{
+					if (formulaChar == '+' || formulaChar == '-') {
+						while (!fh->empty())
+						{
+							hz->push_back(fh->top());
+							fh->pop();
+						}
+						fh->push(FH(formulaChar));
+					}
+					else {
+						hz->push_back(FH(formulaChar));
+					}
+				}
+			}
+		}
+		len++;
+	}
+	hz->push_back(formula.substr(start));
+	while (!fh->empty())
+	{
+		hz->push_back(fh->top());
+		fh->pop();
+	}
+
+	stack<int> result;
+	int a1, a2;
+	for (auto j : *hz)
+	{
+		if (j == "+" || j == "-" || j == "*" || j == "/")
+		{
+			if (j == "+")
+			{
+				a1 = result.top();
+				result.pop();
+				a2 = result.top();
+				result.pop();
+				result.push((a1 + a2));
+			}
+			if (j == "-")
+			{
+				a1 = result.top();
+				result.pop();
+				a2 = result.top();
+				result.pop();
+				result.push((a2 - a1));
+			}
+			if (j == "*")
+			{
+				a1 = result.top();
+				result.pop();
+				a2 = result.top();
+				result.pop();
+				result.push((a1 * a2));
+			}
+			if (j == "/")
+			{
+				a1 = result.top();
+				result.pop();
+				a2 = result.top();
+				result.pop();
+				result.push((a2 / a1));
+			}
+		}
+		else
+			result.push(atoi(j.c_str()));
+	}
+	return formula + "=" + to_string(result.top());
+}
+
+int main() {
+	Calculator *calc = new Calculator();
+	string Problem;
+	int n;
+	cout << "请输入你想要几道题？" << endl;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		Problem = calc->MakeFormula();
+		cout << Problem << endl;
+		cout << calc->Solve(Problem) << endl;
+	}
+	return 0;
+}
