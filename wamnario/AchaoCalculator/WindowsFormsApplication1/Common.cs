3b61bc6cda19989453c22b8a6fd4277a43ef9e8d
wamnario <wamnario@espoir.city>
2019-09-18 14:11:37 +0800

    Question Generator Function First Commit

---
+++ wamnario/AchaoCalculator/WindowsFormsApplication1/Common.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Common
+    {
+        public static int qnumber;
+        public static List<string> list = new List<string>();
+    }
+}
---
+++ wamnario/AchaoCalculator/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void qnum_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void generate_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void output_Click(object sender, EventArgs e)
+        {
+            generate generate = new generate();
+            generate.generatequestion(qnum.Text);
+        }
+    }
+}
