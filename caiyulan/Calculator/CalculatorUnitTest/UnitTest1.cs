bc3afd79fbad59c2beb1d5422cbf84154c122ee8
caiyulan <2484647824@qq.com>
2019-09-18 11:07:52 +0800

    第二次作业

---
+++ caiyulan/Calculator/CalculatorUnitTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Calculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string result = "20+10*5";
+            string res = "70";
+            string TestRes = Program.Calculate(result);
+            Assert.AreEqual(res,TestRes);
+        }
+    }
+}
