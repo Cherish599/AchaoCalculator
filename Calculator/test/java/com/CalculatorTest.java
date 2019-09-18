38115a965b58ca9a99dcc4fff7e73f8ea323228b
chenval <402414521@qq.com>
2019-09-18 13:24:28 +0800

    完成了基本功能

--- Calculator/test/java/com/CalculatorTest.java
+++ Calculator/test/java/com/CalculatorTest.java
@@ -1,86 +1,56 @@
-package java.com;
+package com;
 
-import org.junit.Test; 
-import org.junit.Before; 
+import com.Calculator;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
 import org.junit.After;
 
-/** 
-* com.Calculator Tester.
-* 
-* @author <Authors name> 
-* @since <pre>09/16/2019</pre>
-* @version 1.0 
-*/ 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Calculator Tester.
+ *
+ * @author <Authors name>
+ * @since <pre>09/17/2019</pre>
+ * @version 1.0
+ */
 public class CalculatorTest {
+    Calculator calculator=new Calculator();
+    @Before
+    public void before() throws Exception {
+    }
 
+    @After
+    public void after() throws Exception {
+    }
 
-@Before
-public void before() throws Exception { 
-} 
+    /**
+     * Method: createFormula()
+     */
+    @Test
+    public void testCreateFormula() throws Exception {
+//TODO: Test goes here...
 
-@After
-public void after() throws Exception { 
-} 
+    }
 
-/** 
-* 
-* Method: add(int x, int y) 
-* 
-*/ 
-@Test
-public void testAdd() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /**
+     * Method: solve(List formula)
+     */
+    @Test
+    public void testSolve() throws Exception {
+//TODO: Test goes here...
+        List<Object> a=new ArrayList<>(Arrays.asList(26,"+",97,"/",90,"+",54));
+        Assert.assertEquals(calculator.solve(a),81);
+    }
 
-/** 
-* 
-* Method: sub(int x, int y) 
-* 
-*/ 
-@Test
-public void testSub() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: mul(int x, int y) 
-* 
-*/ 
-@Test
-public void testMul() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: div(int x, int y) 
-* 
-*/ 
-@Test
-public void testDiv() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: div2(int x, int y) 
-* 
-*/ 
-@Test
-public void testDiv2() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: loop(int x, int y) 
-* 
-*/ 
-@Test
-public void testLoop() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-
-} 
+    /**
+     * Method: main(String[] args)
+     */
+    @Test
+    public void testMain() throws Exception {
+//TODO: Test goes here...
+    }
+}
