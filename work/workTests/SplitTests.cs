9390ee2ac93e9318d82c53f7f06f611557d71596
胡思荣 <749963470@qq.com>
2019-09-19 11:12:28 +0800

    第二次提交

---
+++ work/workTests/SplitTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using work;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work.Tests
+{
+    [TestClass()]
+    public class SplitTests
+    {
+        Split s = new Split();
+        [TestMethod()]
+        public void SplitsTest()
+        {
+            List<string> l = s.Splits();
+            Assert.IsNotNull(l);
+        }
+    }
+}
---
+++ work/workTests/getsTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using work;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work.Tests
+{
+    [TestClass()]
+    public class getsTests
+    {
+        gets gets = new gets();
+        Split split = new Split();
+        [TestMethod()]
+        public void Gets1Test()
+        {
+            gets.Gets1(split.Splits());
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Gets2Test()
+        {
+            gets.Gets1(split.Splits());
+            int a = gets.Gets2();
+            Assert.IsNotNull(a);
+        }
+    }
+}
