e671d1d44d231295b4696fc8df969f9e1c14e5c4
sparrow <1352369106@qq.com>
2019-09-19 21:12:53 +0800

    First Commit

---
+++ sparrowstar11/arithmetic/arithmeticTests/CalculateTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using arithmetic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arithmetic.Tests
+{
+    [TestClass()]
+    public class CalculateTests
+    {
+        [TestMethod()]
+        public void calTest()
+        {
+            //创建一个Calculate类型的对象
+            Calculate cal = new Calculate();
+            //测试用例4+2*3
+            List<string> calist = new List<string>();
+            calist.Add("4");
+            calist.Add("+");
+            calist.Add("2");
+            calist.Add("*");
+            calist.Add("3");
+            //此对象调用cal方法
+            int result = cal.cal(calist);
+            //若返回的值跟预期的结果10相同，则证明测试通过
+            Assert.AreEqual(10,result);
+        }
+    }
+}
---
+++ sparrowstar11/arithmetic/arithmeticTests/ConnectTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using arithmetic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arithmetic.Tests
+{
+    [TestClass()]
+    public class ConnectTests
+    {
+        [TestMethod()]
+        public void randomIntTest()
+        {
+            //创建一个Connect类型的对象
+            Connect con = new Connect();
+            //定义一个变量来接收产生的这个随机数
+            int number = con.randomInt(1, 100);
+            //如果产生的这个随机数在[1,100)则测试通过
+            bool result = (number >= 1 && number < 100);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void randomopTest()
+        {
+            //创建一个Connect类型的对象
+            Connect con = new Connect();
+            //定义一个字符变量来接收随机产生的这个运算符
+            string op = con.randomop();
+            //如果产生的这个随机运算符是运算符中的一个，则测试通过
+            bool result = (op == "+" || op == "-" || op == "*" || op == "/");
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void createExpressionTest()
+        {
+            //创建一个Connect类型的对象
+            Connect con = new Connect();
+            List<string> conlist = new List<string>();
+            //声明一个表示运算符个数的变量
+            int count = 3;
+            //如果这个conlist不为空，则证明通过测试
+            conlist = con.createExpression(count);
+            Assert.IsNotNull(conlist);
+        }
+    }
+}
