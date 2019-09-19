f8638057ac8bf82d164a6e5f8250124bf0fbba30
namehousiqi <1318743093@qq.com>
2019-09-19 21:18:30 +0800

    第三次提交

--- namehousiqi/Calculator/MyUnitTest/UnitTest1.cs
+++ namehousiqi/Calculator/MyUnitTest/UnitTest1.cs
@@ -10,9 +10,31 @@ namespace MyUnitTest
         public void TestMethod1()
         {
             Calculator.Calculate calculate = new Calculator.Calculate();
-            calculate.makeExercises(5);
+            calculate.makeExercises(1000);
             Console.WriteLine();
-            calculate.makeExercises(3);
+            calculate.makeExercises(30);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Calculator.Calculate calculate = new Calculator.Calculate();
+            calculate.makeExercises(0);
+            Console.WriteLine();
+            calculate.makeExercises(1);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Calculator.Calculate calculate = new Calculator.Calculate();
+            calculate.makeExercises(500);        
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Calculator.Calculate calculate = new Calculator.Calculate();
+            calculate.makeExercises(-1);
+            calculate.makeExercises(-200);
+            calculate.makeExercises(100);
         }
     }
 }
