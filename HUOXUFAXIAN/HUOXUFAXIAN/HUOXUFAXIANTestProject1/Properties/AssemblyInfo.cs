b6e63e218c8736bee1edc5c9f234f3d0a4647425
HUOXUFAXIAN <55314633+HUOXUFAXIAN@users.noreply.github.com>
2019-09-19 12:27:14 +0800

    HUXUFAXIAN

---
+++ HUOXUFAXIAN/HUOXUFAXIAN/HUOXUFAXIANTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HUOXUFAXIANTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("HUOXUFAXIANTestProject1")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8f65b239-4fee-4079-9458-d80fe78edf07")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HUOXUFAXIAN/HUOXUFAXIAN/HUOXUFAXIANTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HUOXUFAXIANTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            FH f = new FH();
+            int a=1;
+            int actual = FH.fuhao();
+            char excepted = "+";
+            Assert.AreEqual(expected,actual,"输出不正确");
+        }
+    }
+}
