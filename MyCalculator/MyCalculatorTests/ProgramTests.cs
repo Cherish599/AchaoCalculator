c1b8de1a4e72430a9d5a93bf2491872f20946381
xiaxiaoshux <2597759547@qq.com>
2019-09-19 11:13:52 +0800

    新増

---
+++ MyCalculator/MyCalculatorTests/ProgramTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void makequestionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SolveTest()
+        {
+            Program p = new Program();
+            string a = "8*7+1*6-5";
+            string ret = p.Solve(a);
+            Assert.AreEqual(ret, (string)"11+22=33");
+        }
+    }
+}
