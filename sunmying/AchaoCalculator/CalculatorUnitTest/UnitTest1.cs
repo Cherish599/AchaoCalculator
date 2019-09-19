f5be054072d6fcdcbade99049216e597a5d42e5f
sunmying <3049636899@qq.com>
2019-09-19 19:20:01 +0800

    sunmying 第二次作业

---
+++ sunmying/AchaoCalculator/CalculatorUnitTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AchaoCalculator;
+
+namespace CalculatorUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SolveTest()
+        //Solve函数测试
+        { 
+            string msg1 = "53+24-45";
+            string correctAns = "53+24-45=32";
+            string ans = Program.Solve(msg1);
+            Assert.AreEqual(correctAns,ans);
+        }
+    }
+}
