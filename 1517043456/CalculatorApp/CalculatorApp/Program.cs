817a86b18a84239002d5e3aa1b3bbee370ef8bba
1517043456 <1517043456@qq.com>
2019-09-19 12:59:14 +0800

    201731054221

---
+++ 1517043456/CalculatorApp/CalculatorApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入你需要的四则运算数量：");
+            String TotalNum = Console.ReadLine();
+            int num = Convert.ToInt32(TotalNum);
+            Solve s = new Solve();
+            StringBuilder sb = new StringBuilder();
+            WriteInTxt wt = new WriteInTxt();
+            Console.WriteLine("打印数据中，请等待……");
+            //获取题目数量
+            while (num > 0)
+            {
+                string result = s.GetResult();
+                Console.WriteLine(result);
+                sb.Append(result).Append("\r\n");
+                num--;
+            }
+            //写入文件
+            wt.WriteTxt(sb.ToString());
+        }
+    }
+}
---
+++ 1517043456/CalculatorApp/CalculatorApp/WriteInTxt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Calculator
+{
+    public class WriteInTxt
+    {
+        public void WriteTxt(string result)
+        {
+            try
+            {
+                string txtfile = "F:\\subject.txt";
+                StreamWriter sw = new StreamWriter(txtfile);
+                sw.Write(result);
+                sw.Flush();
+                sw.Close();
+                Console.WriteLine("写入F://subject.txt完毕！");
+            }
+            catch (Exception e) {
+                Console.WriteLine("文件写入失败！");
+            }
+        }
+    }
+}
