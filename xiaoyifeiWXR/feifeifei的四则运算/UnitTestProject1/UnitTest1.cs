ca733aa57e824a1e6db766780a0ab6238681a918
xiaoyifeiWXR <1070078478@qq.com>
2019-09-19 18:40:09 +0800

    xiaoyifei

---
+++ xiaoyifeiWXR/feifeifei的四则运算/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using feifeifei的四则运算;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            calculator cc = new calculator();
+        }
+    }
+}
---
+++ xiaoyifeiWXR/feifeifei的四则运算/feifeifei的四则运算/calculator.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace feifeifei的四则运算
+{
+  public  class calculator
+    {
+        public static int right = 0;                          //记录答对的总数！
+        public static int wrong = 0;                          //记录答错的总数！
+
+        public void mathjia()                                 //加法运算！
+        {
+            int a, b;
+            int result;
+            Random rd = new Random();
+            a = rd.Next(0, 11);
+            b = rd.Next(0, 11);
+            Console.WriteLine("请计算：{0}+{1}=？", a, b);
+            result = Convert.ToInt32(Console.ReadLine());
+            if (result == a + b)
+            {
+                Console.WriteLine("回答正确！");
+                right++;
+            }
+            else
+            {
+                Console.WriteLine("错误，继续努力！");
+                wrong++;
+            }
+
+        }
+        public void mathjian()                                //减法运算！
+        {
+            int a, b;
+            int result;
+            Random rd = new Random();
+            a = rd.Next(0, 11);
+            b = rd.Next(0, 11);
+            Console.WriteLine("请计算：{0}-{1}=？", a, b);
+            result = Convert.ToInt32(Console.ReadLine());
+            if (result == a - b)
+            {
+                Console.WriteLine("回答正确！");
+                right++;
+            }
+            else
+            {
+                Console.WriteLine("错误，继续努力！");
+                wrong++;
+            }
+        }
+        public void mathcheng()                               //乘法运算！
+        {
+            int a, b;
+            int result;
+            Random rd = new Random();
+            a = rd.Next(0, 11);
+            b = rd.Next(0, 11);
+            Console.WriteLine("请计算：{0}*{1}=？", a, b);
+            result = Convert.ToInt32(Console.ReadLine());
+            if (result == a * b)
+            {
+                Console.WriteLine("回答正确！");
+                right++;
+            }
+            else
+            {
+                Console.WriteLine("错误，继续努力！");
+                wrong++;
+            }
+
+        }
+        public void mathchu()                                 //除法运算！
+        {
+            int a, b;
+            int result;
+            Random rd = new Random();
+            a = rd.Next(0, 11);
+            b = rd.Next(0, 11);
+            if (b != 0)
+            {
+                Console.WriteLine("请计算：{0}/{1}=？", a, b);
+                result = Convert.ToInt32(Console.ReadLine());
+                if (result == a / b)
+                {
+                    Console.WriteLine("回答正确！");
+                    right++;
+                }
+                else
+                {
+                    Console.WriteLine("错误，继续努力！");
+                    wrong++;
+                }
+
+            }
+
+            else
+            {
+                if (a != 0)
+                {
+                    Console.WriteLine("请计算：{0}/{1}=？", b, a);
+                    result = Convert.ToInt32(Console.ReadLine());
+                    if (result == b / a)
+                    {
+                        Console.WriteLine("回答正确！");
+                        right++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("错误，继续努力！");
+                        wrong++;
+                    }
+                }
+            }
+        }
+        public void result()                                  //统计结果！
+        {
+    Console.WriteLine("总共做了{0}道题：你做对了{1}道题，做错了{2}道题。", right + wrong, right, wrong);
+        }
+    }
+}
+
+
+       
