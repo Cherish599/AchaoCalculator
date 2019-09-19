147653e92a0f4c39be78afa6966e69cce31a4042
sulei1999 <1809494430@qq.com>
2019-09-19 22:54:37 +0800

    succeed

--- sulei1999/第二次/Caculate/stdafx.cpp
+++ sulei1999/第二次/Caculate/stdafx.cpp
@@ -4,5 +4,5 @@
 
 #include "stdafx.h"
 
-// TODO: 在 STDAFX.H 中引用任何所需的附加头文件，
+// TODO:  在 STDAFX.H 中引用任何所需的附加头文件，
 //而不是在此文件中引用
--- sulei1999/第二次/Caculate/stdafx.h
+++ sulei1999/第二次/Caculate/stdafx.h
@@ -9,6 +9,5 @@
 
 // 用于 CppUnitTest 的头文件
 #include "CppUnitTest.h"
-#include "../Caculate/Caculate.h"
 
 // TODO: 在此处引用程序需要的其他头文件
