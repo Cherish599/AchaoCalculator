3fb506f33e3ddf8c1a1d488ae069d9537f9738f1
胡思荣 <749963470@qq.com>
2019-09-19 11:03:37 +0800

    新增

---
+++ work/Newrandow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work
+{
+    public class Newrandow
+    {
+
+        Random random = new Random();
+        string[] symbol = { "+", "-", "*", "/" };
+        public int RandInt()
+        {
+            return random.Next(1, 100);
+        }
+
+        public string RandSymbol()
+        {
+            return symbol[random.Next(0, 4)];
+        }
+
+        public int Randm()
+        {
+            return random.Next(2, 4);
+        }
+    }
+}
---
+++ work/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter file = new StreamWriter(@"D:\workplace\vs\subject.txt");
+            int n = int.Parse(Console.ReadLine());
+            Split split = new Split();
+            gets test = new gets();
+            while(true)
+            {
+                List<string> vs = split.Splits();
+                test.Gets1(vs);
+                int s = test.Gets2();
+                if(s>=0)
+                {
+                    Console.Write(string.Join("", vs.ToArray()));
+                    file.Write(string.Join("", vs.ToArray()));
+                    Console.Write(" = ");
+                    file.Write(" = ");
+                    Console.WriteLine(s);
+                    file.WriteLine(s);
+                    n--;
+                }
+                else
+                {
+                    continue;
+                }
+                if(n==0)
+                {
+                    break;
+                }
+            }
+            file.Flush();
+            file.Close();
+        }
+    }
+}
