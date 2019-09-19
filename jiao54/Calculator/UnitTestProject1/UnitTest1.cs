e62106476343f9ce90e630dfe90592c06b9efdb5
jiao54 <2676087477@qq.com>
2019-09-19 17:58:47 +0800

    第二次作业

---
+++ jiao54/Calculator/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string formula = "32/8+7";
+            string result = "11";
+            string Test_result = Program.Calculate(formula);
+            Assert.AreEqual(result, Test_result);
+        }
+    }
+}
