b64c229724cc2f6ca4cc8048d292b2d6fc26080e
slothph <1114273773@qq.com>
2019-09-19 13:06:17 +0800

    201731062323 彭皓 提交

---
+++ slothph/phCalculator/phCalculator/Test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using phCalculator;
+using System.Data;
+
+namespace phCalculator
+{
+   public class Test
+    {
+        public static string test_timu;
+        //测试题目是否合格，将合格题目加入列表
+        public static Boolean test(string timu)
+        {
+            //有除以0时，直接否决
+            if (timu.Contains("/0"))
+                return false;
+            DataTable dt = new DataTable();
+            object res = dt.Compute(timu, "");
+            //结果为小数或者负数的，也被否决
+            if (Convert.ToInt32(res) < 0 || res.ToString().Contains("."))
+                return false;
+            else
+            {
+                Program.ans.Add(timu + "=" + res.ToString());
+                test_timu = timu + "=" + res.ToString();
+                return true;
+            }
+
+        }
+    }
+}
---
+++ slothph/phCalculator/phCalculatorTests3/TestTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using phCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phCalculator.Tests
+{
+    [TestClass()]
+    public class TestTests
+    {
+        [TestMethod()]
+        public void testTest()
+        {
+            //test1和2用于测试算数结果是否正确 
+            //以及这两个合格题目是否会让test返回true
+            string test1 = "25*4-3+50";
+            int ans1 = 25 * 4 - 3 + 50;
+            Test.test(test1);
+            Assert.AreEqual(Test.test_timu, (test1 + "=" + ans1.ToString()));
+            Assert.IsTrue(Test.test(test1));
+
+            string test2 = "44*6-5";
+            int ans2 = 44 * 6 - 5;
+            Test.test(test2);
+            Assert.AreEqual(Test.test_timu, (test2 + "=" + ans2.ToString()));
+            Assert.IsTrue(Test.test(test2));
+
+            //test3，4,5测试不合格题目是否会让test返回false；
+            string test3 = "5/0+6";
+            string test4 = "8-61*1+5";
+            string test5 = "9/2+9+1";
+            Assert.IsFalse(Test.test(test3));
+            Assert.IsFalse(Test.test(test4));
+            Assert.IsFalse(Test.test(test5));
+
+
+
+
+
+
+        }
+    }
+}
