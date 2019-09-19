1fa549fe0903eb10c05999765793b3e6122e0365
zhangyingzhangying <1604594495@qq.com>
2019-09-19 21:47:38 +0800

    zhangyingzhangying

---
+++ zhangying2/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using zhangying2;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Problem vv = new Problem(1);
+           // vv.Print();
+        }
+    }
+}
