054d6fe519517ce854d9fe15066d11493ee5c13d
coderrrZhou <786657871@qq.com>
2019-09-20 17:58:53 +0800

    zg's project commit

---
+++ zg_cal/GetOp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zg_cal
+{
+    class GetOp
+    {
+
+        public String getOp(int n)
+        {
+            int nums = n;
+            String options = "";
+            for (int i = 0; i < nums; i++)
+            {
+                options += setOp();
+            }
+            return options;
+        }
+
+        public String setOp()
+        {
+            switch (Only.r.Next(0, 4))
+            {
+                case 0:
+                    return "+";
+                case 1:
+                    return "-";
+                case 2:
+                    return "*";
+                case 3:
+                    return "/";
+                default:
+                    return "";
+            }
+        }
+    }
+}
---
+++ zg_cal/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zg_cal
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                //int ques_nums = 1;
+                Console.Write("Do you want to get some(10) new math-questions? ");
+                Console.Write("enter yes or no:");
+                String input = Console.ReadLine();
+                if (input.Equals("yes"))
+                {
+                    Index index = new Index();
+                    for (int i = 0; i < 100; i++)
+                    {
+                        //String ques = "the "+(i+1)+"th problems:";
+                        String ques = index.question();
+                        if (ques == "wrong!")
+                            i--;
+                        else
+                        {
+                            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"E:\achao_cal.txt", true))
+                                {
+                                        file.WriteLine(ques);
+                                }
+                        }
+                    }
+                }
+                else break;
+
+                //Console.WriteLine("success! ");
+            }
+        }
+    }
+}
