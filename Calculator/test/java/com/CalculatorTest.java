545b4407c756af76a345e1ec7c9fda00276589ad
chenval <402414521@qq.com>
2019-09-20 12:25:44 +0800

    四则运算功能已经完成，结果无小数

--- Calculator/test/java/com/CalculatorTest.java
+++ Calculator/test/java/com/CalculatorTest.java
@@ -33,7 +33,7 @@ public class CalculatorTest {
     @Test
     public void testCreateFormula() throws Exception {
 //TODO: Test goes here...
-
+        Assert.assertNotNull(calculator.createFormula());
     }
 
     /**
@@ -43,7 +43,11 @@ public class CalculatorTest {
     public void testSolve() throws Exception {
 //TODO: Test goes here...
         List<Object> a=new ArrayList<>(Arrays.asList(26,"+",97,"/",90,"+",54));
+        List<Object> b=new ArrayList<>(Arrays.asList(25,"-",98,"+",91,"+",54));
+        List<Object> c=new ArrayList<>(Arrays.asList(24,"*",99,"-",92,"+",54));
         Assert.assertEquals(calculator.solve(a),81);
+        Assert.assertEquals(calculator.solve(b),72);
+        Assert.assertEquals(calculator.solve(c),2338);
     }
 
     /**
