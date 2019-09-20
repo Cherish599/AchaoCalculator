8055ce8faa638f6c68084594362deecada100233
Ayo-Kong <408311844@qq.com>
2019-09-20 14:31:09 +0800

    First Commit

---
+++ Ayo-Kong/Secondwork/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //测试运算结果是否正确
+            string a = "10+5/5*4";
+            string b = "10+5/5*4=14";
+            string c = ConsoleApp1.Inputs.R(a);
+            Assert.AreEqual(b, c);
+       
+        }
+    }
+}
---
+++ Ayo-Kong/Secondwork/四则运算/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+namespace ConsoleApp1
+{
+    public class Inputs
+    {
+        public static string Operation()
+        {
+            char[] O = { '+', '-', '*', '/' };//将运算符载入数组中          
+            Random ra = new Random();//生成随机数
+            int a = ra.Next(0, 100);
+            int num = ra.Next(2, 4);
+            string result = null;
+            result = result + a;
+            for (int y = 0; y < num; y++)
+            {
+                int o = ra.Next(0, 4);//在这四个数中随机，实现运算符的随机
+                int b = ra.Next(0, 100);
+                if (O[o] == '/')
+                {
+                    if (b != 0)
+                        result = result + O[o] + b;
+                    else
+                        break;
+                }
+                else
+                    result = result + O[o] + b;
+
+            }
+            return result;
+
+        }
+        public static string R(string T)
+        {
+            object ob = null;
+            DataTable dt = new DataTable();
+            ob = dt.Compute(T, "");
+            while (ob.ToString().Contains(".") || int.Parse(ob.ToString()) < 0)//判断四则运算是否符合题目要求
+            {
+                T = Operation();
+                ob = dt.Compute(T, "");//再次检验是否合乎要求
+            }
+            return T + "=" + ob.ToString();
+        }
+
+    }
+}
