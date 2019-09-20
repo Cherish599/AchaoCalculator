7e660bac6599d9d1d6c71e8144c5ce0f824607f2
blackboy19991 <1621632649@qq.com>
2019-09-20 18:22:01 +0800

    First commit

---
+++ blackboy19991/周成杰/周成杰Tests/ProblemTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using 周成杰;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 周成杰.Tests
+{
+    [TestClass()]
+    public class ProblemTests
+    {
+        [TestMethod()]
+        public void ProblemTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PrintTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void WritewTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
