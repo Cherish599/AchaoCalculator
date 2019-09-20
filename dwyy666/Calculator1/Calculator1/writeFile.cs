41cbe604c76dad7fc26f7c916a1e0563a688f3b9
unknown <dwyy666@126.com>
2019-09-20 11:25:04 +0800

    魏家田 201731062307

---
+++ dwyy666/Calculator1/Calculator1/writeFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator1
+{
+   public class writeFile
+    {
+        public void writefile(string str)
+        {
+
+            try
+            {
+                StreamWriter sw = new StreamWriter("e:\\test.txt", true);
+                sw.WriteLine(str);
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("文件写入失败！");
+            }
+
+            
+        }
+    }
+}
