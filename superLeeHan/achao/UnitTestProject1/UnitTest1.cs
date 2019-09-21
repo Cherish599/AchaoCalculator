92d395287a9825b397e72f4e18ea2f263e4beea4
superLeeHan <18228050501@163.com>
2019-09-21 14:47:17 +0800

    AchaoCalculator

---
+++ superLeeHan/achao/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void verifyTest()
+        {
+            Assert.IsTrue(Calculator.verify("22+5*20") == 122.ToString());
+        }
+    }
+}
